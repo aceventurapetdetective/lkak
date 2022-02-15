5bdfc82a12466d0151cf66f8af5ba85a04a7423b
RaviharaV <91451271+RaviharaV-bot@users.noreply.github.com>
2022-02-15 12:33:37 +0530

    Update multifish.sh

--- multifish.sh
+++ multifish.sh
@@ -24,7 +24,7 @@ cd Stockfish-master/src
 if [ "$(g++ -Q -march=native --help=target | grep mpopcnt | grep enabled)" ] ; then
   make profile-build ARCH=x86-64-modern COMP=gcc
   strip stockfish
-  mv stockfish ../../stockfish_x64_modern
+  mv stockfish ../../multisf
   make clean
 fi
 
@@ -37,3 +37,5 @@ fi
 #fi
 
 cd
+
+./multisf bench antichess 16 1 13
